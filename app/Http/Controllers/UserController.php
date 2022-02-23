e903f75dbcd7d80eb330ae505a950c7b92971bdf
sglkc <31957516+sglkc@users.noreply.github.com>
2022-02-23 20:40:54 +0700

    init commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        $user = User::create([
+            'username' => $request->input('username'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response()->json($user);
+    }
+}
